b37b2978f3f8f662ad58abc45a477aac844e1c88
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2019-11-12 13:50:17 -0500

    clase 9 - comparaciones

---
+++ clase9.js
@@ -0,0 +1,14 @@
+var x = 4, y = "4"
+
+var sacha = {
+    nombre: "Sacha"
+}
+
+var otraPersona = {
+    nombre: "Sacha"
+}
+
+// Js tiene distintas maneras de comparar con == compara si tienen el 
+// mismo valor sin importar el tipo (4 == “4” // true) y con === cuando 
+// comparamos si son idénticos los valores (4 === “4” //false )
+//  CUANDO COMPARAMOS OBJETOS SE COMPARA REFERENTE A LA VARIABLE DEL OBJETO
